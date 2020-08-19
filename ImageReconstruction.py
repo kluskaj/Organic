a613fd631fba8e5d6a077865b80606292e9b10ce
RikClaes <rikclaes@live.be>
2020-08-19 17:08:02 +0200

    hoping for no overwrite

--- ImageReconstruction.py
+++ ImageReconstruction.py
@@ -4,25 +4,28 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.optimizers import Adam
 import numpy as np
 import FunctionLibrary as lib
+import tensorflow as tf
+
 ################################################################################
 ################ parameters for the reconstruction #############################
 ################################################################################
-discPath = os.path.expandvars('${VSC_DATA}/CNN/unifCosi_basicGAN_multi4/saved_model_test/discriminatorGAN_CNN_test.h5')
+discPath = os.path.expandvars('${VSC_DATA}/summerjobTests/test1/saved_models/discriminatorfinalModel.h5')
 discriminator = load_model(discPath)
 
 
-genPath = os.path.expandvars('${VSC_DATA}/CNN/unifCosi_basicGAN_multi4/saved_model_test/generatorGAN_CNN_test.h5')
+genPath = os.path.expandvars('${VSC_DATA}/summerjobTests/test1/saved_models/generatorfinalModel.h5')
 Generator= load_model(genPath)
-hyperParam = 2
-Begin_avraging =9000
-image_Size = 256
+hyperParam = 0.00000002
+Begin_avraging =200
+image_Size = 128
 NoiseLength = 150
 RandomWalkStepSize = 0.5
-alterationInterval = 500
-plotinterval = 3000
-epochs = 30000
-optimizer=Adam(learning_rate=0.0001,amsgrad=False)
-#in order to load artificial datasets in a numpy format
+alterationInterval = 100
+plotinterval = 100 #set to a multiple of the alteration interval to see the effect of noise vector change
+epochs = 10200
+optimizer=Adam(learning_rate=0.0001,beta_1=0.91,beta_2=0.999,amsgrad=False)
+
+#load artificial datasets in a numpy format
 dirV2 =os.path.expandvars('${VSC_DATA}/CNN/TrainingDoubleLossGen/V2ModelImage6_gausianNoise_IRAS08544baselines.npy')
 simV2 = np.load(dirV2)
 dirCP =os.path.expandvars('${VSC_DATA}/CNN/TrainingDoubleLossGen/CPModelImage6_gausianNoise_IRAS085442baselines.npy')
@@ -47,11 +50,11 @@ filename = 'IRAS08544-4431_PIONIER_alloidata.fits'
 x = -0.44   #the right-ascention of a point source star, to be removed using sparco
 y = -0.68   #the declination of a point source star, to be removed using sparco
 UDflux =59.7 #the flux contribution of the a central resolved star, represented as a uniform disk (set to 0 for an unresloved point source)
-PointFlux = 3.9 # The flux contribution of a point source star
+PointFlux = 0#3.9 # The flux contribution of a point source star
 denv = 0.42 # the spectral index of the environment
 dsec = -2, #  the spectral index of the point source star (the uniform disk source has a default index of 4)
 UDdiameter = 0.5 # the diameter of the resolved source
-pixelSize = 0.2734375 #pixel size in mas (currently same as thesis, yields an fov for an imagesize of 256)
+pixelSize = 0.2734375*2 #pixel size in mas (currently same as thesis, yields an fov for an imagesize of 256)
 
 dataLikelihood = lib.dataLikeloss_FixedSparco(DataDir,filename,image_Size,
 x,
@@ -60,20 +63,20 @@ UDflux,
 PointFlux,
 denv,
 dsec,
-UDdiameteraryDiameter,
+UDdiameter,
 pixelSize,
-V2Artificial = None,CPArtificial = None)
+V2Artificial = simV2,CPArtificial = simCP)
 
 ################################################################################
 ################## run  the image reconstrution ################################
 ################################################################################
 
-mean, Image, diskyLoss, fitLoss = lib.reconsruction(Generator, discriminator,optimizer,dataLikelihood , epochs ,image_Size ,hyperParam,NoiseLength,Begin_avraging ,RandomWalkStepSize,alterationInterval,plotinterval,saveDir  = '')
+mean, varianceImage, diskyLoss, fitLoss = lib.reconsruction(Generator, discriminator,optimizer,dataLikelihood ,pixelSize, epochs ,image_Size ,hyperParam,NoiseLength,Begin_avraging ,RandomWalkStepSize,alterationInterval,plotinterval,saveDir  = '')
 
 
 # store the mean and variance image
 np.save('meanImage',mean)
-np.save('varianceImage',Image)
+np.save('varianceImage',varianceImage)
 #store the numpy arrays for further use
 np.save('diskyLoss',diskyLoss)
 np.save('fitLoss',fitLoss)
@@ -88,12 +91,15 @@ dataLikelihood = lib.dataLikeloss_FixedSparco(DataDir,filename,image_Size,
     PointFlux,
     denv,
     dsec,
-    UDdiameteraryDiameter,
+    UDdiameter,
     pixelSize,
     forTraining = False
 )
 #alter the output range  convert the mean to a tensorflow objectand, give the correct shape
-img = (mean*2)-1 
+img = (mean*2)-1
 img = tf.constant(img)
 img = tf.reshape(img,[1,image_Size,image_Size,1])
-print(dataLikelihood(None,img).numpy())
+lossValue, V2loss , CPloss = dataLikelihood(None,img)
+print(lossValue.numpy())
+print(V2loss.numpy())
+print(CPloss.numpy())
